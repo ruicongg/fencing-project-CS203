6e881b4387974322ab720374faa542c4cc49158c
ruicong <kwanruicong6462@gmail.com>
2024-09-19 23:07:30 +0800

    Remove unnecessary CRUD folder

--- CRUD project/src/main/java/com/example/demo/tournaments/TournamentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.tournaments;
-
-import org.springframework.stereotype.Repository;
-import org.springframework.data.repository.CrudRepository;
-
-@Repository
-public interface TournamentRepository extends CrudRepository<Tournament, Long> {
-
-}
