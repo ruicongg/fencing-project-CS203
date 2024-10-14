624b3aa5dadab08fb3e9d19de38f6ad631e6e87e
ruicong <kwanruicong6462@gmail.com>
2024-10-14 10:28:15 +0800

    removed @valid in tournamentservice for clarity

--- src/main/java/org/fencing/demo/stages/GroupStage.java
+++ src/main/java/org/fencing/demo/stages/GroupStage.java
@@ -28,8 +28,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 
 @Data
 @AllArgsConstructor
