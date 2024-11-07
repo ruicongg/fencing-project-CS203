580112a9c849b730401d8cf421020c426de9e45a
toshiyo <134270931+Toshiyo08@users.noreply.github.com>
2024-11-07 14:58:35 +0800

    playernotfound

--- backend/src/main/java/org/fencing/demo/exception/PlayerNotFoundException.java
+++ backend/src/main/java/org/fencing/demo/exception/PlayerNotFoundException.java
@@ -8,14 +8,6 @@ public class PlayerNotFoundException extends RuntimeException{
     private static final long serialVersionUID = 1L;
 
     public PlayerNotFoundException(Long id){
-<<<<<<<< HEAD:backend/src/main/java/org/fencing/demo/player/PlayerNotFoundException.java
         super ("Could not find Player with ID " +id);
-    }
-
-    public PlayerNotFoundException(String username){
-        super ("Could not find Player with username " +username);
-========
-        super ("Could not find Player " +id);
->>>>>>>> feature/elo-update:backend/src/main/java/org/fencing/demo/exception/PlayerNotFoundException.java
     }
 }
