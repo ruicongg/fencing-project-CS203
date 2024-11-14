95d28d0e8f7e5f45454658cc817f621b06fd3afb
toshiyo <134270931+Toshiyo08@users.noreply.github.com>
2024-11-14 09:27:13 +0800

    Added controllers to map frontend

---
+++ backend/src/main/java/org/fencing/demo/controller/SPAController.java
@@ -0,0 +1,14 @@
+package org.fencing.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class SPAController {
+
+    @RequestMapping(value = "/{path:[^\\.]*}")
+    public String redirect() {
+        return "forward:/index.html";
+    }
+}
+
